b8e6033db6ec2227a0adfb80b091b1c8c6603a60
THARU67 <156172188+THARU67@users.noreply.github.com>
2024-07-19 21:32:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=2Zc3FBpS#0FLBFpvPQd9tOp7F2bXyCFXXN_A89d8R-5mkwcx7Drc' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
